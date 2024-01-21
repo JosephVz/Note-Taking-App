a21786ab297b0ac98877efa6707bba03687ad7d5
unknown <vanzandt.school.email@gmail.com>
2024-01-21 14:52:35 -0500

    Installed uuid to create unique ids and created the api routes to post and get.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,21 +1,35 @@
 
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/api/notes', async (req, res) => {
-    const data = await JSON.parse(fs.readFileSync('db/db.json'));
-    res.json(data);
+    const dbJson = await JSON.parse(fs.readFileSync('db/db.json', "utf8"));
+    res.json(dbJson);
 });
 
 router.post('/api/notes', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('db/db.json'));
+    const dbJson = JSON.parse(fs.readFileSync('db/db.json', "utf8"));
     const newNote = {
         title: req.body.title,
         text: req.body.text,
-        // id:
+        id: uuidv4(),
     };
 
     data.push(newNote);
     fs.writeFileSync('db/db.json', JSON.stringify(data));
     res.json(data);
 });
+
+// router.delete('/api/note/:id', (req, res) => {
+//     let data = fs.readFileSync('db/db.json', "utf8")
+//     const dataJSON = JSON.parse(data);
+//     const newNotes = dataJSON.filter((note) => {
+//         return note.id !== req.params.id;
+//     });
+
+//     fs.writeFileSync("db/db.json", JSON.stringify(newNotes));
+//     res.json("Deleted Note.")
+// });
+
+module.exports = router;
