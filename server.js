aab60cca7b9efcf1b72fd1a69d02ec818fa4b52c
unknown <vanzandt.school.email@gmail.com>
2024-01-21 14:15:44 -0500

    server.js is added with basic code routing to files yet to exist.

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const htmlroute = require('./routes/htmlroutes');
+const apiroutes = require('./routes/apiroutes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(htmlroutes);
+app.use(apiroutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}`);
+});
